0a26f1856858903fe54b08c2837ba4a6d41e8353
sajjad0057 <sajjadhossain0057@gmail.com>
2021-08-24 16:29:00 +0600

    create blog list pages and generate class based  pagination

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,6 @@ app_name = 'blog'
 
 urlpatterns = [
 
-    path('',views.Bloglist,name="blog-list"),
-    # path('',views.BlogList.as_view(),name="blog-list"),
+    # path('',views.Bloglist,name="blog-list"),
+    path('',views.BlogList.as_view(),name="blog-list"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,24 +5,24 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
-# class BlogList(ListView):
-#     model = Blog
-#     paginate_by = 3
-#     context_object_name = 'blog'
-#     template_name = 'blog.html'
+class BlogList(ListView):
+    model = Blog
+    paginate_by = 3
+    context_object_name = 'blog'
+    template_name = 'blog.html'
 
 
 
-def Bloglist(request):
-    blogs = Blog.objects.all()
-    page = request.GET.get('page')
+# def Bloglist(request):
+#     blogs = Blog.objects.all()
+#     page = request.GET.get('page')
 
-    paginator = Paginator(blogs,4)
-    try:
-        blog = paginator.page(page)
-    except PageNotAnInteger:
-        blog = paginator.page(1)
-    except EmptyPage:
-        blog = paginator.page(paginator.num_pages)
+#     paginator = Paginator(blogs,4)
+#     try:
+#         blog = paginator.page(page)
+#     except PageNotAnInteger:
+#         blog = paginator.page(1)
+#     except EmptyPage:
+#         blog = paginator.page(paginator.num_pages)
 
-    return render(request, 'blog.html', { 'blog': blog })
+#     return render(request, 'blog.html', { 'blog': blog })
