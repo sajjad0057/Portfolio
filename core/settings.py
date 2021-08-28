df8dc154d40c3e1f4e5b74fe3963e9d718434e35
sajjad0057 <sajjadhossain0057@gmail.com>
2021-08-29 04:09:57 +0600

    update settings.py

--- core/settings.py
+++ core/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4ua+9t%l-&9iayudpp!n6kfo9jq2y(&qimlmyu_ub-h!8zcdp7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'sajjad-hossain.herokuapp.com',
